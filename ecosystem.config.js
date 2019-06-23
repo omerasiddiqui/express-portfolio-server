63f12241d9ad0dd131c48b8c0339b116334a6c14
Omer Siddiqui <omersiddiqui@Omers-MacBook-2.local>
2019-06-23 15:44:50 -0600

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+      name: 'portfolio',
+      script: './index.js'
+    }],
+    deploy: {
+      production: {
+        user: 'ubuntu',
+        host: 'ec2-18-218-238-227.us-east-2.compute.amazonaws.com',
+        key: '~/.ssh/portfolio.pem',
+        ref: 'origin/master',
+        repo: 'https://github.com/omerasiddiqui/express-portfolio-server.git',
+        path: '/home/ubuntu/server',
+        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      }
+    }
+  }
