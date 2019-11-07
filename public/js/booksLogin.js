d5c448ac04b3d4ed00a25aa3627eda04586f2bdf
Siddiqui <Omer.Siddiqui@CenturyLink.com>
2019-11-07 11:41:32 -0700

    adding books login page

--- public/js/booksLogin.js
+++ public/js/booksLogin.js
@@ -34,6 +34,8 @@ console.log('Hello from the login books js file!');
 
   var loginBtn = document.getElementById('loginBtn');
   var logoutBtn = document.getElementById('logoutBtn');
+  var googleLogIn = document.getElementById('googleLoginBtn');
+
 
   // log in the user
   loginBtn.addEventListener('click', function() {
@@ -91,5 +93,49 @@ console.log('Hello from the login books js file!');
         console.log(userJsonData);
     }
   })
+
+  // Google sign in
+
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  googleLogIn.addEventListener('click', () => {
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+      console.log(user);
+      console.log(token);
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+      console.log(`Error Code: ${errorCode}, error message: ${errorMessage}, error email: ${email}, credentials: ${credential}.`)
+    });
+  })
+
+  // change persistence to session baased
+  firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+  .then(function() {
+    // Existing and future Auth states are now persisted in the current
+    // session only. Closing the window would clear any existing state even
+    // if a user forgets to sign out.
+    // ...
+    // New sign-in will be persisted with session persistence.
+    return firebase.auth().signInWithEmailAndPassword(email, password);
+  })
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+  
+
 
     
