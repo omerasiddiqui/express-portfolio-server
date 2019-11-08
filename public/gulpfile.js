f2fef2abd036bb386217fc198b53be52f0a6d6e9
Omer Siddiqui <omersiddiqui@Omers-MacBook-2.local>
2019-11-07 17:48:47 -0700

    minify css file

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -2,7 +2,9 @@ const gulp = require('gulp'),
           sass = require('gulp-sass'),
           browserSync = require('browser-sync').create(),
           imagemin = require('gulp-imagemin'),
-          print = require('gulp-print').default();
+          print = require('gulp-print').default(),
+          cleanCSS = require('gulp-clean-css');
+
 
 
 // Compile Sass
@@ -14,6 +16,13 @@ gulp.task('sass', function(done) {
     stream: true
   }))
   done();
+});
+
+// minify css
+gulp.task('minifyCss', () => {
+  return gulp.src('css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./css/'));
 });
 
 // Optimize Images
@@ -33,7 +42,7 @@ gulp.task('browserSync', function(){
 });
 
 // Watch Task
-gulp.task('watch', gulp.series(['browserSync', 'sass'], function(){
+gulp.task('watch', gulp.series(['browserSync', 'sass', 'minifyCss'], function(){
   gulp.watch('sass/**/*.scss', ['sass'], browserSync.reload);
   gulp.watch('js/**/*.js', browserSync.reload);
   gulp.watch('./*.html', browserSync.reload);
