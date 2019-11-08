ebb172de3be9d7634de6447a22bbbe89fd83100f
Omer Siddiqui <omersiddiqui@Omers-MacBook-2.local>
2019-11-07 17:01:24 -0700

    changing ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       production: {
         user: 'ubuntu',
         host: 'ec2-18-218-238-227.us-east-2.compute.amazonaws.com',
-        key: '~/.ssh/portfolio2.pem',
+        key: '~/.ssh/portfolio.pem',
         ref: 'origin/master',
         repo: 'https://github.com/omerasiddiqui/express-portfolio-server.git',
         path: '/home/ubuntu/server',
